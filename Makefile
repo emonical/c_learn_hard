154c496e594c300743f87b9554bfd63652998bbf
Edward Monical-Vuylsteke <solrane.altari@gmail.com>
2011-11-23 18:06:52 -0600

    Chapter 8 - Exercises

---
+++ Makefile
@@ -0,0 +1,8 @@
+CFLAGS=-Wall -g
+
+.PHONY: clean
+
+clean:
+	rm -f ex8
+	rm -f ex1
+	rm -Rf *.dSYM
---
+++ ex8.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+  char full_name[] = { 
+                     'z', 'e', 'd', ' ',
+                     'A', '.', ' ',
+                     'S', 'h', 'a', 'w', '\0' 
+  };
+  int areas[] = { 10, 12, 13, 14, 20 };
+  char name[] = "Zed";
+
+  printf("The size of an int: %ld\n", sizeof( int ));
+  printf("The size of areas (int[]): %ld\n", sizeof ( areas ));
+  printf("The number of ints in areas: %ld\n", sizeof ( areas ) / sizeof ( int ));
+  printf("The first area is %d, the second %d.\n", areas[0], areas[1]);
+
+  printf("The size of a char: %ld\n", sizeof ( char ));
+  printf("The size of name (char[]): %ld\n", sizeof ( name ));
+  printf("The number of chars: %ld\n", sizeof ( name ) / sizeof ( char ));
+
+  printf("The size of full_ame (char[]): %ld\n", sizeof ( full_name ));
+  printf("The number of chars: %ld\n", sizeof ( full_name ) / sizeof ( char ));
+
+  printf("name=\"%s\" and full_name=\"%s\"\n", name, full_name);
+
+  return 0;
+}
